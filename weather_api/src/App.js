4b0ddf88c3e937f5fe13e050f6f8dc24cf753160
TRIF3X <hoessly.conner@gmail.com>
2019-03-23 22:19:56 -0500

    add positioning

--- weather_api/src/App.js
+++ weather_api/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Input } from 'semantic-ui-react';
 import Button from './Components/weatherButton.js';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="Home_main">
-        Welcome to Weather_API
+        <h2>Welcome to Weather_API</h2>
+        <div className='Home_firstDiv'>
         <Input placeholder='City or Zip code' />
         {Button('green', 'Get weather')}
+        </div>
       </div>
     );
   }
