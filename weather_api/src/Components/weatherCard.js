595218df668ed962e4e0abf85f600886489b927a
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-30 14:05:32 -0500

    card details filled in

--- weather_api/src/Components/weatherCard.js
+++ weather_api/src/Components/weatherCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 
 const date = new Date()
 const today = date.toLocaleDateString("en-US", {
@@ -14,8 +14,17 @@ const time = date.toLocaleTimeString("en-US", {
 const WeatherCard = (props) => (
     <Card>
         <Card.Content>
+            <Image floated='right' size='mini' src={`http://openweathermap.org/img/w/${props.data.weather[0].icon}.png`} />
             <Card.Header>{props.data.name}</Card.Header>
             <Card.Meta>{props.data.weather[0].description}</Card.Meta>
+        </Card.Content>
+        <Card.Content>
+            <Card.Header textAlign='center'>Details</Card.Header>
+            <Card.Content>Current {props.data.main.temp} &#x2109;</Card.Content>
+            <Card.Content>High {props.data.main.temp_max} &#x2109;</Card.Content>
+            <Card.Content>Low {props.data.main.temp_min} &#x2109;</Card.Content>
+            <Card.Content>Wind {props.data.wind.speed} mph</Card.Content>
+            <Card.Content>Humidity {props.data.main.humidity}%</Card.Content>
         </Card.Content>
         <Card.Content extra>
             {today}  {time}
